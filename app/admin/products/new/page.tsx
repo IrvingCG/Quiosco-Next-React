6ce433b39a0529ac85b3856f06853262ac66ed6c
IrvingCG <antio30_96@utlook.com>
2024-12-29 21:51:38 -0600

    Validar campos vacios al registrar prodictos

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -1,9 +1,17 @@
+import Heading from "@/components/ui/Heading";
+import AddProductForm from "@/components/products/AddProductForm";
+import ProductForm from "@/components/products/ProuctForm";
 
 export default function CreateProductPage() {
-
-
-
   return (
- <h1>ProductsPage</h1>
+    <>
+     <Heading>Nuevo producto</Heading>
+  
+      <AddProductForm>
+        <ProductForm/>
+      </AddProductForm>  
+    
+    </>
+
   )
 }
