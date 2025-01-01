d1c3df9bd4a7f8fa0d69bcaaad655ad171410dfe
IrvingCG <antio30_96@utlook.com>
2025-01-01 14:55:11 -0600

    CREAR PRODUCTOS Y REGISTRAR A LA BASE DE DATOS

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -29,9 +29,7 @@ export const SearchSchema = z.object({
 })
 
 export const ProductSchema = z.object({
-    name: z.string()
-        .trim()
-        .min(1, { message: 'El Nombre del Producto no puede ir vacio'}),
+    name: z.string().trim().min(1, { message: 'El Nombre del Producto no puede ir vacio'}),
     price: z.string()
         .trim()
         .transform((value) => parseFloat(value)) 
@@ -42,4 +40,6 @@ export const ProductSchema = z.object({
         .transform((value) => parseInt(value)) 
         .refine((value) => value > 0, { message: 'La Categoría es Obligatoria' })
         .or(z.number().min(1, {message: 'La Categoría es Obligatoria' })),
+        image: z.string().min(1,{message:'La Imagen es obligatoria'})
+        
 })
